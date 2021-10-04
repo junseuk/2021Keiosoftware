be82556b83f4131e92674f863ef7da904b20b685
junseuk <junseuk86@gmail.com>
2021-10-04 20:18:53 +0900

    changed from BST to simple sorted array

--- hashvsbinary.c
+++ hashvsbinary.c
@@ -3,18 +3,14 @@
 #include <assert.h>
 #include <time.h>
 
+int SIZE;
+int numData;
 //HashTable
 struct bucket* hashTable = NULL;
-int SIZE;
-//Binary Tree
-struct node* root = NULL;
 
 struct node{
     int key;
-    int value;
     struct node* next;
-    struct node* left;
-    struct node* right;
 };
 
 struct bucket {
@@ -22,16 +18,12 @@ struct bucket {
     int count;
 };
 
-struct node* createNode(int key, int value) {
+struct node* createNode(int key) {
     struct node* newNode;
     newNode = (struct node*)malloc(sizeof(struct node));
 
     newNode -> key = key;
-    newNode -> value = value;
-
     newNode -> next = NULL;
-    newNode -> left = NULL;
-    newNode -> right = NULL;
 
     return newNode;
 }
@@ -40,9 +32,9 @@ int hashFunction(int key) {
     return key%SIZE;
 }
 
-void insertHash(int key, int value){
+void insertHash(int key){
     int hashIndex = hashFunction(key);
-    struct node* newNode = createNode(key, value);
+    struct node* newNode = createNode(key);
     //No node at hashIndex
     if (hashTable[hashIndex].count == 0){
         hashTable[hashIndex].head = newNode;
@@ -57,121 +49,114 @@ void insertHash(int key, int value){
     return;
 }
 
-void insertBinary(int key, int value) {
-    struct node* p = root;
-    struct node* parent = NULL;
-    if (p == NULL) {
-        root = createNode(key, value);
-        return;
-    }
-    while (p!=NULL) {
-        parent = p;
-        if (p -> value == value) {
-            //printf("[ Binary Search ] Already existed value in the tree\n");
-            return;
-        }
-        else if (value < p -> value) {
-            p = p -> left;
-        }
-        else {
-            p = p -> right;
+int partition (int array[numData], int start, int end) {
+    int pivot = array[(start + end)/2];
+    while (start <= end) {
+        while(array[start] < pivot) start++;
+        while(array[end] > pivot) end--;
+        if (start <= end) {
+            int tmp = array[start];
+            array[start] = array[end];
+            array[end] = tmp;
+            start++;
+            end--;
         }
     }
-    if (parent != NULL) {
-        if (value < parent -> value) {
-            parent -> left = createNode(key, value);
-        }
-        else {
-            parent -> right = createNode(key, value);
-        }
-    }
-
+    return start;
 }
 
+void quickSort(int array[numData], int start, int end) {
+    int secondStart = partition(array, start, end);
+    if ( start < secondStart - 1) {
+        quickSort(array, start, secondStart - 1);
+    }
+    if ( secondStart < end ) {
+        quickSort(array, secondStart, end );
+    }
+}
 
-int searchHash(int key) {
-    //printf("inside searchHash key: %d", key);
+void searchHash(int key) {
     int searchCount = 0;
     int hashIndex = hashFunction(key);
     struct node* node = hashTable[hashIndex].head;
     if (node == NULL) {
         printf("[ Hash Search ] NOT FOUND\n");
-        return -1;
+        return;
     }
     while(node != NULL) {
         searchCount++;
         if (node -> key == key) {
-            printf("[  Hash Search  ] FOUND! Key: %d, Value: %d\n", key, node -> value);
+            printf("[  Hash Search  ] FOUND! Key: %d\n", key);
             printf("[  Hash Search  ] Search Count: %d\n", searchCount);
-            return node -> value;
+            return;
         }
         node = node -> next;
     };
-    return -1;
+    return;
 }
 
-void searchBinary(int value) {
+void searchBinary(int key, int array[SIZE]) {
+    int start = 0;
+    int end = numData - 1;
     int searchCount = 0;
-    struct node* p = root;
-    while (p!=NULL) {
+    while(1) {
+        if (start > end) {
+            printf("[ binary Search ] NOT FOUND\n");
+            return;
+        }
         searchCount++;
-        if (p -> value == value) {
-            printf("[ Binary Search ] FOUND! Key: %d, Value: %d\n", p -> key, value);
+        int mid = (start + end) / 2;
+        if ( array[mid] == key ) {
+            printf("[ Binary Search ] FOUND! Key: %d\n", key);
             printf("[ Binary Search ] Search Count: %d\n", searchCount);
             return;
         }
-        else if (value < p -> value) {
-            p = p -> left;
+        else if ( key < array[mid] ) {
+            end = mid;
+            continue;
         }
         else {
-            p = p -> right;
+            start = mid + 1;
+            continue;
         }
     }
-    printf("[ Binary Search ] NOT FOUND!\n");
-    printf("[ Binary Search ] Search Count: %d\n", searchCount);
-    return;
 }
 
-void insert(int key, int value) {
-    insertHash(key, value);
-    insertBinary(key, value);
-    return;
-}
-
-void search(int key) {
-    int value = searchHash(key);
-    searchBinary(value);
+void search(int key, int array[SIZE]) {
+    searchHash(key);
+    searchBinary(key, array);
     return;
 }
 
 
 int main() {
-    char buffer[1000];
-    unsigned int numData;
+    char buffer[100];
     srand(time(0));
     //SIZE of hash table
     printf("Please decide the size of hash table: ");
     fgets(buffer, sizeof(buffer), stdin);
-    sscanf(buffer, "%u", &SIZE);
+    sscanf(buffer, "%d", &SIZE);
     assert(SIZE>0);
     hashTable = (struct bucket*) malloc(SIZE*sizeof(struct bucket));
-
     //Number of Data
     printf("Please input the number of data: ");
     fgets(buffer, sizeof(buffer), stdin);
-    sscanf(buffer, "%u", &numData);
+    sscanf(buffer, "%d", &numData);
+    //Create sorted array and hash table
+    int array[numData];
     for (int i=0;i<numData;i++) {
         int randValue = rand();
-        insert(i, randValue);
-        printf("%d, %d\n", i, randValue);
+        insertHash(randValue);
+        array[i] = randValue;
+        printf("Inserted Key: %d\n", randValue);
     }
+    quickSort(array, 0, numData - 1);
     
     //Key to search
     int key = 0;
     printf("Input the key to search: ");
     fgets(buffer, sizeof(buffer), stdin);
     sscanf(buffer, "%d", &key);
-    assert(key<numData);
     assert(key>=0);
-    search(key);
+    search(key, array);
 }
